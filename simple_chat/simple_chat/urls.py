83da6803b13df09010454cb3c5f5f133a11c7e9c
Pavel <dem-pasha@lenta.ru>
2022-08-27 22:44:14 +0300

    nothing

--- simple_chat/simple_chat/urls.py
+++ simple_chat/simple_chat/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),  # includes url mapping from the chat application
