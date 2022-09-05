571e6e6a1c805e19814357f855c2f7dd2279aecc
Pavel <dem-pasha@lenta.ru>
2022-09-06 00:53:05 +0300

    Added many features to the design.

--- simple_chat/simple_chat/settings.py
+++ simple_chat/simple_chat/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
-django_heroku.settings(locals())
+
 
